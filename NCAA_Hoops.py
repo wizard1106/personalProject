2f75cc015648e450ecc1e2934131432c3eeada6c
paul <paultadam5@outlook.com>
2024-04-19 17:21:53 -0500

    failed attemp to make faster. Also deleted old python script

--- NCAA_Hoops.py
+++ NCAA_Hoops.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 21 11:36:09 2024
+Created on Mon Mar 18 15:32:50 2024
 
 @author: pauladam
-"""
-# -*- coding: utf-8 -*-
-"""
-@author: Chad.Birger
 """
 
 import pandas as pd
 import datetime as dt
 import requests
 import time
-import sqlite3
 from urllib.error import HTTPError
-from sqlalchemy import create_engine, text
-import pyodbc
-
-
-username = 'paultadam5_outlook.com#EXT#@paultadam5outlook.onmicrosoft.com'
-password = 'CloudSAb948c5f2'
-hostname = 'ncaa-hoops.database.windows.net'
-database_name = 'ncaa-hoops'
-driver= '{SQL Server}'
-
-
-conn_str = f'DRIVER={driver};SERVER=tcp:{hostname};PORT=1433;DATABASE={database_name};UID={username};PWD={password}'
-engine = create_engine(f"mssql+pyodbc://?odbc_connect={conn_str}",use_setinputsizes=False)
-connection = engine.connect()
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
@@ -66,29 +47,33 @@ for row in game_id:
 game_ids = []
 for row in gameId:
     game_ids.append(list(row))
-    
-#The following is an example to pull one-game worth of data: 
-game = game_ids[69][4]
-url = 'https://stats.ncaa.org/contests/' +str(game)+'/box_score' 
-df_box_score = pd.read_html(url)
-time.sleep(1)
-headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
-page = requests.get(url, headers=headers)
-game_info = page.text
-g = game_info.find('<a href="/game/play_by_play/')
-if g < 0:
-    print("not found")
-game2 = game_info[g+28: g+35]
-
-url2 = 'https://stats.ncaa.org/game/play_by_play/' +str(game2)
-df_pxp = pd.read_html(url2)
-time.sleep(1)
-url3 =  'https://stats.ncaa.org/game/period_stats/' +str(game2)
-df_period_stats = pd.read_html(url3)
-time.sleep(1)
 
 
-#=====================================================================
+from sqlalchemy import create_engine, text
+import pyodbc
+print(pyodbc.drivers())
+
+#print("connected")
+username = 'ncaa-hoops'
+password = 'paulZachChadServer!'
+hostname = 'ncaa-hoops.database.windows.net'
+database_name = 'ncaa-hoops'
+#driver= '{SQL Server}'
+driver =  '{ODBC Driver 18 for SQL Server}'
+conn_str = f'DRIVER={driver};SERVER=tcp:{hostname};PORT=1433;DATABASE={database_name};UID={username};PWD={password}'
+engine = create_engine(f"mssql+pyodbc://?odbc_connect={conn_str}",use_setinputsizes=False)
+
+connection = engine.connect()
+
+
+def create_tables(gameId:int,game2:int,bx:list[pd.DataFrame],pxp:list[pd.DataFrame],ps:list[pd.DataFrame]) -> None:
+    for i,df in enumerate(bx):
+        df.to_sql(f'b{i}x{gameId}',connection)
+    for i,df in enumerate(pxp):
+        df.to_sql(f'p{i}xp{game2}',connection)
+    for i,df in enumerate(ps):
+        df.to_sql(f'p{i}s{game2}',connection)
+
 game_list=[]
 game_list2= []
 game_results_box_score = []
@@ -97,209 +82,44 @@ game_results_period_stats = []
 no_games = []
 for row in game_ids:
     for gameId in row:
+        print("gameID:", gameId)
         try:
             url =  'https://stats.ncaa.org/contests/' + str(gameId) + '/box_score'
-            #url = 'ncaa.com/game/' + str(gameId)
             df_box_score = pd.read_html(url)
             time.sleep(3)
-            headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
+            headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.37"}
             page = requests.get(url, headers=headers)
             game_info = page.text
             g = game_info.find('<a href="/game/play_by_play/')
             if g < 0:
                 print("not found")
             game2 = game_info[g+28: g+35]
+            print("game2:", game2)
             url2 = 'https://stats.ncaa.org/game/play_by_play/' +str(game2)
             df_pxp = pd.read_html(url2)
             time.sleep(3)
             url3 =  'https://stats.ncaa.org/game/period_stats/' +str(game2)
             df_period_stats = pd.read_html(url3)
             time.sleep(3)
-            game_results_box_score.append(df_box_score)
-            game_results_pxp.append(df_pxp)
-            game_results_period_stats.append(df_period_stats)
             game_list.append(gameId)
-        except ValueError:
+            create_tables(gameId,game2,df_box_score,df_pxp,df_period_stats)
+            print('success')
+        except ValueError as er:
+            print('\tVerror')
+            print(er)
             no_games.append(gameId)
             pass
-#=====================================================================
-
-
-
-
-
-#=====================================================================
-
-
-
-
-
-#=====================================================================
-game_list=[]
-game_list2= []
-game_results_box_score = []
-
-game_results_pxp = []
-
-game_results_period_stats = []
-
-no_games = []
-
-for row in game_ids:
-    for gameId in row:
-        print(gameId)
-        try:
-            url = 'https://stats.ncaa.org/contests/' + str(gameId) + '/box_score'
-    
-    #url = 'ncaa.com/game/' + str(gameId)
-    
-            df_box_score = pd.read_html(url)
-            
-            time.sleep(3)
-            
-            headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.37"}
-            
-            page = requests.get(url, headers=headers)
-            
-            game_info = page.text
-            
-            g = game_info.find('<a href="/game/play_by_play/')
-            
-            if g < 0:
-            
-                print("not found")
-            
-            game2 = game_info[g+28: g+35]
-            
-            url2 = 'https://stats.ncaa.org/game/play_by_play/' +str(game2)
-                    
-            df_pxp = pd.read_html(url2)
-            
-            time.sleep(3)
-            
-            url3 = 'https://stats.ncaa.org/game/period_stats/' +str(game2)
-            
-            df_period_stats = pd.read_html(url3)
-            
-            time.sleep(3)
-
-#since we are creating sql tables, we no longer need the lists
-
-#this way seems better for memory
-
-#game_results_box_score.append(df_box_score)
-
-#game_results_pxp.append(df_pxp)
-
-#game_results_period_stats.append(df_period_stats)
-
-        game_list.append(gameId)
-        create_tables(gameId,game2,df_box_score,df_pxp,df_period_stats)
-        print('success')
-        except ValueError:
-        print('\tVerror')
-        no_games.append(gameId)
-        pass
         except HTTPError as err:
-        print('HttpError')
-        no_games.append(gameId)
-        pass
+            print('HttpError')
+            no_games.append(gameId)
+            pass
+'''        except Exception as err:
+            print('reg',err)
+            no_games.append(gameId)
+            pass'''
 
 connection.close()
-
 engine.dispose()
-
-#I think .close and .displose is right, haven't tested it, shouldn't matter
-
-
-
-
-
-
-
-
-#=====================================================================
-# df_box_score.to_csv('df_box_score.csv', index=False)
-# df_period_stats.to_csv('df_period_stats.csv', index=False)
-# df_pxp.to_csv('df_pxp.csv', index=False)
-# game_id.to_csv('game_id.csv', index=False)
-# game_ids.to_csv('game_ids.csv', index=False)
-# gameId.to_csv('gameId.csv', index=False)
-# games_today.to_csv('games_today.csv', index=False)
-
-# for i, df in enumerate(df_box_score):
-#     df.to_csv(f'df_box_score_{i}.csv', index=False)
-
-# for i, df in enumerate(df_period_stats):
-#     df.to_csv(f'df_period_stats_{i}.csv', index=False)
-
-# for i, df in enumerate(df_pxp):
-#     df.to_csv(f'df_pxp_{i}.csv', index=False)
-    
-# #Running into issues getting these into csv
-# for i, df in enumerate(game_id):
-#     df.to_csv(f'game_id_{i}.csv', index=False)
-
-# for i, df in enumerate(game_ids):
-#     df.to_csv(f'game_ids_{i}.csv', index=False)
-
-# for i, df in enumerate(gameId):
-#     df.to_csv(f'gameId_{i}.csv', index=False)
-
-# for i, df in enumerate(games_today):
-#     df.to_csv(f'games_today_{i}.csv', index=False)
-
-
-
-
-
-# #============================================================================
-
-
-# # Create or connect to an SQLite database file
-# conn = sqlite3.connect('ncaaHoops.db')
-
-# # Assume df_box_score, df_period_stats, etc. are lists of DataFrames
-# # Iterate over each DataFrame in the lists and store them in the database
-# for i, df_list in enumerate([df_box_score, df_period_stats, df_pxp, game_id, game_ids, gameId, games_today]):
-#     for j, df in enumerate(df_list):
-#         table_name = f'table_{i+1}_{j+1}'  # Create a unique table name for each DataFrame
-#         df.to_sql(table_name, conn, index=False)  # Store the DataFrame in the database as a table
-
-# # Commit changes and close connection
-# conn.commit()
-# conn.close()
-
-# print("DataFrames have been stored in the SQLite database.")
-
-# #============================================================================
-
-# try:
-#     conn = sqlite3.connect('ncaaHoops.db')
-
-#     # Assume df_box_score, df_period_stats, etc. are lists of DataFrames
-#     # Iterate over each DataFrame in the lists and store them in the database
-#     for i, df_list in enumerate([df_box_score, df_period_stats, df_pxp]):
-#         for j, df in enumerate(df_list):
-#             table_name = f'table_{i+1}_{j+1}'  # Create a unique table name for each DataFrame
-#             df.to_sql(table_name, conn, index=False)  # Store the DataFrame in the database as a table
-
-#     # Commit changes and close connection
-#     conn.commit()
-#     conn.close()
-
-#     print("DataFrames have been stored in the SQLite database.")
-
-# except Exception as e:
-#     print("An error occurred:", e)
-
-
-
-
-
-
-
-# # , game_id, game_ids, gameId, games_today
 
 
 
